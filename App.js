2b93967f547bf46e0744e3bada0b05e80be080c7
art-off <a.rylov@id-east.ru>
2021-11-04 23:58:12 +0700

    slkd

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  * @flow strict-local
  */
 import React, { useState, useEffect } from 'react'
-import {AsyncStorage, Text} from 'react-native'
+import { AsyncStorage, Text } from 'react-native'
 import Navigation from './services/navigation/HomeScreen'
 import AppLoading from 'expo-app-loading'
 import { useFonts } from '@use-expo/font'
@@ -19,68 +19,70 @@ import { AuthManager, useUser } from './services/auth/AuthManager'
 import messaging from '@react-native-firebase/messaging';
 
 
-function App(){
+function App() {
 
-  const  {isAuthorizated} = useUser();
+  return <Text>sldkfjsldkj</Text>
 
-  Text.defaultProps = Text.defaultProps || {};
-  Text.defaultProps.allowFontScaling = false; 
+  // const  {isAuthorizated} = useUser();
 
-  const [firstLaunch, setFirstLaunch] = useState(null)
-  useEffect(() => {
-    const checkFirstLaunch = async () => {
-      const agreement = await AsyncStorage.getItem('agreement');
-      if (agreement) {
-        setFirstLaunch(false)
-      } else {
-        setFirstLaunch(true)
-        await AsyncStorage.removeItem('User');
-        await AsyncStorage.removeItem('AuthData');
-      }
-    }
+  // Text.defaultProps = Text.defaultProps || {};
+  // Text.defaultProps.allowFontScaling = false; 
 
-    checkFirstLaunch()
-  }, [])
+  // const [firstLaunch, setFirstLaunch] = useState(null)
+  // useEffect(() => {
+  //   const checkFirstLaunch = async () => {
+  //     const agreement = await AsyncStorage.getItem('agreement');
+  //     if (agreement) {
+  //       setFirstLaunch(false)
+  //     } else {
+  //       setFirstLaunch(true)
+  //       await AsyncStorage.removeItem('User');
+  //       await AsyncStorage.removeItem('AuthData');
+  //     }
+  //   }
 
-  // Устанавливаем кастомный шрифт, который лежит в ./assets/fonts/
-  let [fontsLoaded] = useFonts({
-    'roboto': require('./assets/fonts/18811.ttf'),
-  });
+  //   checkFirstLaunch()
+  // }, [])
 
-  // Если шрифты еще не были установлены, продолжаем загружать приложение
-  if (!fontsLoaded || firstLaunch === null) {
-    return <AppLoading />;
-  }
+  // // Устанавливаем кастомный шрифт, который лежит в ./assets/fonts/
+  // let [fontsLoaded] = useFonts({
+  //   'roboto': require('./assets/fonts/18811.ttf'),
+  // });
 
-  // Проверяем наличие UUID в хранилище. Если его нет, то генерируем и записываем
-  AsyncStorage.getItem('UUID')
-  .then(res => {
-    if (res === null){
-      AsyncStorage.setItem('UUID', Math.random().toString(36).substr(2, 8) + '-' + Math.random().toString(36).substr(2, 4) + '-' + 
-      Math.random().toString(36).substr(2, 4) + '-' + Math.random().toString(36).substr(2, 4) + '-' + 
-      Math.random().toString(36).substr(2, 12))
-    }
-  })
+  // // Если шрифты еще не были установлены, продолжаем загружать приложение
+  // if (!fontsLoaded || firstLaunch === null) {
+  //   return <AppLoading />;
+  // }
 
-  AsyncStorage.getItem('@mode')
-    .then(res => {
-      if (res === null)
-        AsyncStorage.setItem('@mode', '0')
-    })
+  // // Проверяем наличие UUID в хранилище. Если его нет, то генерируем и записываем
+  // AsyncStorage.getItem('UUID')
+  // .then(res => {
+  //   if (res === null){
+  //     AsyncStorage.setItem('UUID', Math.random().toString(36).substr(2, 8) + '-' + Math.random().toString(36).substr(2, 4) + '-' + 
+  //     Math.random().toString(36).substr(2, 4) + '-' + Math.random().toString(36).substr(2, 4) + '-' + 
+  //     Math.random().toString(36).substr(2, 12))
+  //   }
+  // })
 
-  return (
-    <AuthManager>
-      <WeekManager>
-        <AppearanceProvider>
-          <LocaleManager>
-            <ThemeManager>
-              <Navigation firstLaunch={firstLaunch}/>
-            </ThemeManager>
-          </LocaleManager>
-        </AppearanceProvider> 
-      </WeekManager> 
-    </AuthManager>
-  )
+  // AsyncStorage.getItem('@mode')
+  //   .then(res => {
+  //     if (res === null)
+  //       AsyncStorage.setItem('@mode', '0')
+  //   })
+
+  // return (
+  //   <AuthManager>
+  //     <WeekManager>
+  //       <AppearanceProvider>
+  //         <LocaleManager>
+  //           <ThemeManager>
+  //             <Navigation firstLaunch={firstLaunch}/>
+  //           </ThemeManager>
+  //         </LocaleManager>
+  //       </AppearanceProvider> 
+  //     </WeekManager> 
+  //   </AuthManager>
+  // )
 }
 
 export default App;
--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-import { registerRootComponent } from 'expo';
+// import { registerRootComponent } from 'expo';
 import App from './App';
 import { AppRegistry } from 'react-native';
 import messaging from '@react-native-firebase/messaging';
 import axios from 'axios'
 import { BASE_URL } from './modules/constants';
 
-axios.defaults.baseURL=BASE_URL
+axios.defaults.baseURL = BASE_URL
 
-// Register background push notifications
-messaging().setBackgroundMessageHandler(async remoteMessage => {
-  console.log('Message handled in the background!', remoteMessage);
-});
-
-
-// Subscribe to topic for notifications
-messaging()
-  .subscribeToTopic('allUsers_android')
-  .then(() => console.log('Subscribed to topic!'));
-
-messaging()
-  .subscribeToTopic('debug_android')
-  .then(() => console.log('Subscribed to topic!'));
+// // Register background push notifications
+// messaging().setBackgroundMessageHandler(async remoteMessage => {
+//   console.log('Message handled in the background!', remoteMessage);
+// });
 
 
+// // Subscribe to topic for notifications
+// messaging()
+//   .subscribeToTopic('allUsers_android')
+//   .then(() => console.log('Subscribed to topic!'));
 
-AppRegistry.registerComponent('main', () => App);
+// messaging()
+//   .subscribeToTopic('debug_android')
+//   .then(() => console.log('Subscribed to topic!'));
 
-registerRootComponent(App)
+
+
+AppRegistry.registerComponent('mysibsu', () => App);
+
+// registerRootComponent(App)
 
